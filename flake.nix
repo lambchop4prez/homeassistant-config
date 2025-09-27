e4f5bdbe9f9edb2e467ba77bd0be346959870390
Tim <lambchop4prez@users.noreply.github.com>
2025-09-27 18:24:39 -0400

    feat: New devices, nix devShell, packages for device config

--- flake.nix
+++ flake.nix
@@ -16,37 +16,46 @@
                 pkgs = import nixpkgs {
                     inherit system;
                 };
-                python = pkgs.python312;
-                pythonPackages = python.pkgs;
-                lib-path = with pkgs; lib.makeLibraryPath [
-                    libffi
-                    openssl
-                    stdenv.cc.cc
-                ];
+                # python = pkgs.python312;
+                # pythonPackages = python.pkgs;
+                # lib-path = with pkgs; lib.makeLibraryPath [
+                #     libffi
+                #     openssl
+                #     stdenv.cc.cc
+                # ];
             in
             {
                 default = pkgs.mkShell {
-                    packages = [
-                        pythonPackages.pillow
-                        # pythonPackages.esphome
+                    packages = with pkgs; [
+                        # (python312Full.withPackages (
+                        #     ps: with ps; [
+                        #         pip
+                        #         jmespath
+                        #         requests
+                        #         setuptools
+                        #         pyyaml
+                        #         pyopenssl
+                        #     ]
+                        # ))
+                        esphome
                     ];
 
-                    shellHook = ''
-                        SOURCE_DATE_EPOCH=$(date +%s)
-                        export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
-                        VENV=.venv
+                    # shellHook = ''
+                    #     SOURCE_DATE_EPOCH=$(date +%s)
+                    #     export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
+                    #     VENV=.venv
 
-                        if test ! -d $VENV; then
-                            python3.12 -m venv $VENV
-                        fi
-                        source ./$VENV/bin/activate
-                        export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
-                        # pip install -r requirements.txt
-                    '';
+                    #     if test ! -d $VENV; then
+                    #         python3.12 -m venv $VENV
+                    #     fi
+                    #     source ./$VENV/bin/activate
+                    #     export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+                    #     pip install -r requirements.txt
+                    # '';
 
-                    postShellHook = ''
-                        ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
-                    '';
+                    # postShellHook = ''
+                    #     ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
+                    # '';
                 };
             }
         );
