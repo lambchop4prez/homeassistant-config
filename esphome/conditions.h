7fb4be47577376cd6b5282f928b77ab47506286a
Tim <lambchop4prez@users.noreply.github.com>
2026-01-25 12:26:50 -0500

    feat: Add trmnl display and local config

---
+++ esphome/conditions.h
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+static std::map<string, string> condition_icons = {
+    {"clear-night", "\U000F0594"},
+    {"cloudy", "\U000F0590"},
+    {"fog", "\U000F0591"},
+    {"hail", "\U000F0592"},
+    {"lightning", "\U000F0593"},
+    {"lightning-rainy", "\U000F067E"},
+    {"partlycloudy", "\U000F0595"},
+    {"pouring", "\U000F0596"},
+    {"rainy", "\U000F0597"},
+    {"snowy", "\U000F0F36"},
+    {"snowy-rainy", "\U000F067F"},
+    {"sunny", "\U000F0599"},
+    {"windy", "\U000F059D"},
+    {"windy-variant", "\U000F059E"},
+    {"exceptional", "\U000F0F38"}
+};
